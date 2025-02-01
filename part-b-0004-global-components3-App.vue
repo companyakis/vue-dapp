528d7784a21da51ed4434cdffe80c85d868a1d2c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-01 22:20:19 +0300

    Create part-b-0004-global-components3-App.vue

---
+++ part-b-0004-global-components3-App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <!-- global component usage! -->
+
+  <app-header/>
+
+  <div class="content">
+    <p>Mustafa Büyükdereli, Vue and Web3 in 2025</p>
+  </div>
+
+
+</template>
+
