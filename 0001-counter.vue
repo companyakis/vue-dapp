bec81668798cc6447026df2bdbbb148db798ae3a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-01 20:07:27 +0300

    Create 0001-counter.vue

---
+++ 0001-counter.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+  import {ref} from "vue"
+
+  const counter = ref(0)
+
+  function counterUp() {
+    counter.value++
+  }
+
+  function counterDown() {
+    counter.value--
+  }
+
+</script>
+
+<template>
+
+  <div>
+    <button @click="counterUp">Counter ++</button>
+  </div>
+
+  <h1>Counter value: {{counter}}</h1>
+  
+  <div>
+    <button @click="counterDown">Counter --</button>
+  </div>
+
+</template>
