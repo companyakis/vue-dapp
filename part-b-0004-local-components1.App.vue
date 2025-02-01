4875925941e6541f02f7339ea2fd83eabcea22ab
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-01 22:25:45 +0300

    Create part-b-0004-local-components1.App.vue

---
+++ part-b-0004-local-components1.App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+  // let's import our local component, Footer
+
+  import Footer from "@/components/Fixed/Footer.vue"
+
+</script>
+
+<template>
+
+  <!-- global component usage! -->
+
+  <app-header/>
+
+  <div class="content">
+    <p>Mustafa Büyükdereli, Vue and Web3 in 2025</p>
+  </div>
+
+  <!-- local component usage! -->
+
+  <Footer />
+
+</template>
+
