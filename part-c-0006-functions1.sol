49fca4ee735f6bcbce40decedfe5eb1dff32e3fe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-02 01:04:58 +0300

    Update part-c-0006-functions1.sol

--- part-c-0006-functions1.sol
+++ part-c-0006-functions1.sol
@@ -5,3 +5,16 @@ Store this uint value inside a public storage variable called x.
 
 */
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+
+    uint public x;
+
+    constructor(uint _x) {
+
+        x = _x;
+    }
+
+}
