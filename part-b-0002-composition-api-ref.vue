48cb7f6a79e455dfec0d2aa7a47d43081e374877
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-01 20:10:32 +0300

    Create part-b-0002-composition-api-ref.vue

---
+++ part-b-0002-composition-api-ref.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+
+  import { ref } from "vue"
+
+  let taxRate = ref(0.18)
+
+  let initilaSalary = 45000
+
+  let salaryTL = ref(initilaSalary)
+
+
+  function salaryUp(increase: number) {
+
+    salaryTL.value += increase
+  }
+
+  function netSalary() {
+
+    salaryTL.value -= salaryTL.value * taxRate.value
+  }
+
+</script>
+
+
+<template>
+
+  <section>
+
+    <h1>Step 1:</h1>
+
+    <h3>Initial net salaryTL: {{initilaSalary}}</h3>
+    
+  </section>
+
+  <section>
+
+    <h1>Step 2:</h1>
+
+    <button @click="salaryUp(15000)">Calculate Gross Salary Increase</button>
+
+  </section>
+
+  <section>
+
+    <h1>Step 3:</h1>
+
+    <button @click="netSalary">Calculate New net Salary</button>
+
+    <div>
+      New net salary {{salaryTL}}
+    </div>
+
+  </section>
+
+</template>
