7bcb9f5a0eb9b55556e8fbc740db4f107768415b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-01 20:11:18 +0300

    Create part-b-0003-composition-api-reactive.vue

---
+++ part-b-0003-composition-api-reactive.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+  import { reactive } from "vue"
+
+  let person = reactive({
+    name: "Aygün Bilge",
+    department: "Sales",
+    salaryTL:  55000
+  })
+
+  function updatePersonInfo() {
+    person.name = "Aygün Büyükdereli",
+    person.department = "Operations",
+    person.salaryTL = 62000
+  }
+
+</script>
+
+
+<template>
+
+  <div>
+    <div>Person name: {{person.name}}</div>
+    <div>Person department: {{person.department}}</div>
+    <div>Person salary ₺: {{person.salaryTL}}</div>
+  </div>
+
+  <div>
+    <button @click="updatePersonInfo">Update Person Data</button>
+  </div>
+
+</template>
