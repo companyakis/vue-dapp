97ae2336622fe312bce340964b2e90ab631a4bc1
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-02 10:37:02 +0300

    Create part-b-0006-css-global-App.vue

---
+++ part-b-0006-css-global-App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+  import Footer from "@/components/Fixed/Footer.vue"
+
+</script>
+
+<template>
+
+  <app-header/>
+
+  <div class="content">
+    <p>Mustafa Büyükdereli, Vue and Web3 in 2025</p>
+  </div>
+
+  <Footer />
+
+</template>
+
+<!-- global css
+don't use "scoped" -->
+
+<style>
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
+
