2b10e7e5f3fff427174e0c01956281579b86e567
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-03 00:22:25 +0300

    Update part-c-0008-sending-ether01-owner.sol

--- part-c-0008-sending-ether01-owner.sol
+++ part-c-0008-sending-ether01-owner.sol
@@ -5,7 +5,19 @@ Next create a constructor function which will store the msg.sender in owner
 
 */
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Contract {
+
+    address public owner;
+
+    constructor() {
+
+        owner = msg.sender;
+    }
+    
+}
 
 
 
