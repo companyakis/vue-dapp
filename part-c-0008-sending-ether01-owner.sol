2f9490c5e32f75c0a32ea557f099bdac9f079e1b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-03 00:22:13 +0300

    Create part-c-0008-sending-ether01-owner.sol

---
+++ part-c-0008-sending-ether01-owner.sol
@@ -0,0 +1,27 @@
+/*
+Your Goal: Store the Owner
+Create a public address state variable called owner on the contract
+Next create a constructor function which will store the msg.sender in owner
+
+*/
+
+
+
+
+
+/*
+An address on the EVM is a 160 bits long, or a 40 character, hexadecimal string:
+
+Ethereum Messages 
+If an Externally Owned Account (EOA) wants to interact with the Ethereum Network, it broadcasts a transaction. Included in this transaction is data, designed to instruct the Ethereum Virtual Machine (EVM).
+
+This data, commonly known as calldata, specifies actions for the EVM to execute and identifies a target contract account. The target may, in turn, initiate calls to other contract accounts. Each of these calls is considered a message call and carries information like the sender's address, the targeted function signature, and the amount of wei being sent.
+
+In Solidity we have access to these message through global variables:
+
+msg.data (bytes) - the complete calldata
+msg.sender (address) - the address sending the message
+msg.sig (bytes4) - the targeted function signature
+msg.value (uint) - the amount of wei sent
+
+*/
