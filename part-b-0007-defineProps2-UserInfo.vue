80f500c1880e81591b853843004efa964b475251
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-02 19:45:18 +0300

    Create part-b-0007-defineProps2-UserInfo.vue

---
+++ part-b-0007-defineProps2-UserInfo.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+    const props =defineProps(["name", "userid", "department", "title", "year"])
+
+</script>
+
+<template>
+
+    <div>User Information</div>
+
+    <ul>
+        <li>User name: {{ name }}</li>
+        <li>User ID: {{ userid }}</li>
+        <li>User department: {{ department }}</li>
+        <li>User title: {{ title }}</li>
+        <li>Year: {{ year }}</li>
+    </ul>
+
+</template>
