aebb53f32efb9a2dda14d2d6ffbf158a53017593
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-02 10:31:55 +0300

    Create part-c-0005-css-scoped-Header.vue

---
+++ part-c-0005-css-scoped-Header.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+  
+    <div>
+        <h2>AI and Web3</h2>
+        <a href="#">Find Me!</a>
+    </div>
+
+</template>
+
+<!-- scoped! -->
+
+<style scoped>
+
+    div {
+        width: 100%;
+        height: 60px;
+        background-color: gray;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    h2 {
+        margin-left: 10px;
+    }
+
+    a {
+        margin-right: 8px;
+        color: wheat;
+        text-decoration: none;
+    }
+
+</style>
