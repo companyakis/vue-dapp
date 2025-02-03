9b02af7c79fdde42925199ab1da018f1ce720bb9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-03 10:22:48 +0300

    Update part-c-0009-revert-require01.sol

--- part-c-0009-revert-require01.sol
+++ part-c-0009-revert-require01.sol
@@ -7,8 +7,17 @@ If at least 1 ether is not sent to the constructor, revert the transaction.
 
 */
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Contract {
 
+    constructor() payable {
+
+        require(msg.value >= 1 ether, "At least, 1 ether deposit!");
+    }
+    
+}
 
 
 /*
