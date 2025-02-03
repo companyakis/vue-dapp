1caf61218e0efba2bc77d93f05fcf0c034857626
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-03 09:16:00 +0300

    Create part-c-0008-sending-ether03-transferring-funds.sol

---
+++ part-c-0008-sending-ether03-transferring-funds.sol
@@ -0,0 +1,49 @@
+/*
+Your Goal: Transfer Tips
+Let's create a way to tip the contract owner!
+
+Create a public payable function tip which sends any of its received ether to the owner.
+
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+Transferring Funds
+
+We can make any regular function payable. This allows us to differentiate the purpose of the ether coming into the smart contract.
+
+Perhaps a contract has two stored addresses and we want to be able to pay each:
+
+contract Contract {
+    address public a;
+    address public b;
+    
+    constructor(address _a, address _b) {
+        a = _a;
+        b = _b;
+    }
+
+    function payA() public payable {
+        (bool s, ) = a.call{ value: msg.value }("");
+        require(s);
+    }
+
+    function payB() public payable {
+        (bool s, ) = b.call{ value: msg.value }("");
+        require(s);
+    }
+}
+
+ We have two pay methods payA and payB which will transfer ether to the respective address. 
+ It takes a uint amount of Wei and transfers it from the contract account to the address.
+
+
+*/
