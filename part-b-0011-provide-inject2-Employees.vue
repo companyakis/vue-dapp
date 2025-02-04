f145d0aa76c0ad73cb4c284fde7cca1d0ca077d3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-04 22:54:30 +0300

    Create part-b-0011-provide-inject2-Employees.vue

---
+++ part-b-0011-provide-inject2-Employees.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+    import EmployeeList from '@/components/EmployeeData/EmployeeList.vue';
+
+</script>
+
+<template>
+
+    <div>
+        <EmployeeList />
+    </div>
+
+</template>
