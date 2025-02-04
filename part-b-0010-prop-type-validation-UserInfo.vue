8b45cbf1b9d578f5503a4f9fd9151c5ca70254d6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-04 20:54:11 +0300

    Create part-b-0010-prop-type-validation-UserInfo.vue

---
+++ part-b-0010-prop-type-validation-UserInfo.vue
@@ -0,0 +1,42 @@
+<script setup>
+const props = defineProps({
+  name: String,
+  userid: {
+    type: String,
+    required: true,
+    default: "000"
+  },
+  department: String,
+  title: String,
+  year: Number,
+  salary: {
+    type: Number,
+    required: true,
+    validator(s) {
+        if (s < 70000) {return false}
+        else {return true}
+    }
+  },
+});
+
+const emit = defineEmits(["update-department"]);
+
+const updateDepartment = () => {
+  emit("update-department", "Credits");
+};
+</script>
+
+<template>
+  <div>User Information</div>
+
+  <ul>
+    <li>User name: {{ name }}</li>
+    <li>User ID: {{ userid }}</li>
+    <li>User department: {{ department }}</li>
+    <li>User title: {{ title }}</li>
+    <li>User salary: {{ salary }} ₺</li>
+    <li>Year: {{ year }}</li>
+  </ul>
+
+  <button @click="updateDepartment">Update Department</button>
+</template>
