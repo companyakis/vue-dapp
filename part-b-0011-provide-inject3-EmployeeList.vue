d5a96c8648cc97ecdeb043e5f4b5adb870b48434
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-04 22:55:48 +0300

    Create part-b-0011-provide-inject3-EmployeeList.vue

---
+++ part-b-0011-provide-inject3-EmployeeList.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+    import { inject } from 'vue';
+
+    const {employees, updateSalary} = inject("employees")
+
+</script>
+
+<template>
+
+    <div>
+        Employee Data: Name and Salary
+    </div>
+    <div>
+        <ul>
+            <li v-for="emp in employees" :key="emp.id">
+
+                {{ emp.name }} => {{ emp.salary }}
+
+            </li>
+        </ul>
+
+        <button @click="updateSalary">Promote Bengü</button>
+    </div>
+
+</template>
