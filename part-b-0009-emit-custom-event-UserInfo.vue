ae2b8f6bd84d742b1afed26a3aaedc21ac28cd57
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-04 20:28:23 +0300

    Create part-b-0009-emit-custom-event-UserInfo.vue

---
+++ part-b-0009-emit-custom-event-UserInfo.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+    const props =defineProps({name: String, userid: String, department: String, title: String, year: Number, salary: Number})
+
+    const emit = defineEmits(["update-department"])
+
+    const updateDepartment = () => {
+        emit("update-department", "Credits")
+    }
+
+</script>
+
+<template>
+
+    <div>User Information</div>
+
+    <ul>
+        <li>User name: {{ name }}</li>
+        <li>User ID: {{ userid }}</li>
+        <li>User department: {{ department }}</li>
+        <li>User title: {{ title }}</li>
+        <li>User salary: {{ salary }} ₺</li>
+        <li>Year: {{ year }}</li>
+    </ul>
+
+    <button @click="updateDepartment">Update Department</button>
+
+</template>
