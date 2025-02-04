2a01201af3cbe63e0ede14fc5923868eb0f18c0a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-04 18:57:28 +0300

    Create part-b-0008-prop-types-App.vue

---
+++ part-b-0008-prop-types-App.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+  import Footer from "@/components/Fixed/Footer.vue"
+
+  import User from "@/components/UserData/UserInfo.vue"
+
+  import { reactive } from "vue"
+
+  const userOne = reactive({
+    userName: "Mustafa",
+    userID: 101,
+    department: "FinTech",
+    title: "Head",
+    year: 2025,
+    salaryTL: 100000
+  })
+
+  function updateTitle() {
+    userOne.title = "Founder"
+  }
+
+</script>
+
+<template>
+
+  <app-header/>
+
+  <div class="content">
+    <p>Mustafa Büyükdereli, Vue and Web3 in 2025</p>
+  </div>
+
+  <div>
+    <!-- data binding -->
+
+    <User :name="userOne.userName" :userid="userOne.userID" :department="userOne.department" :title="userOne.title" :year="userOne.year" :salary="userOne.salaryTL" />
+
+    <div>
+      <button @click="updateTitle">Update User Title</button>
+    </div>
+  </div>
+
+  <!-- <Footer /> -->
+
+</template>
+
+<style>
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
+
