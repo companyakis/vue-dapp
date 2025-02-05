7fafed96b4a46680cde53fc7027012f099671134
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-05 13:53:25 +0300

    Create part-b-0014-lifecycle-hooks3-LifeCycHooks.vue

---
+++ part-b-0014-lifecycle-hooks3-LifeCycHooks.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+import { onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, ref } from 'vue';
+
+onBeforeMount(() => {console.log("First step!")})
+
+onMounted(() => {console.log("Second step!")})
+
+onBeforeUpdate(() => console.log("onBeforeUpdate worked!") )
+
+onUpdated(() => {console.log("onUpdated worked!")})
+
+onBeforeUnmount(()=>{
+    console.log('onBeforeUnmount')
+ })
+
+ onUnmounted(()=>{
+    console.log('onUnmounted')
+ })
+
+const salary = ref(70000)
+
+function salaryIncrease() {
+    salary.value = 95000
+}
+
+</script>
+
+<template>
+
+<div>İnitial salary: {{ salary }}</div>
+
+<div>
+    <button @click="salaryIncrease">Update Salary</button>
+</div>
+
+</template>
