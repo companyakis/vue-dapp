eb41694d2acf0004829206e17309b0d1241d3af3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-05 13:41:25 +0300

    Create part-b-0014-lifecycle-hooks1-App.vue

---
+++ part-b-0014-lifecycle-hooks1-App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import LifeCycHooks from "@/components/HooksExample/LifeCycHooks.vue"
+
+//import { ref } from 'vue';
+
+</script>
+
+<template>
+
+  <LifeCycHooks />
+  
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
