e5a0284490d7227b60834d1e2296a1f54a4f8387
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-05 14:06:03 +0300

    Create part-b-0014-lifecycle-hooks3-App.vue

---
+++ part-b-0014-lifecycle-hooks3-App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import LifeCycHooks from "@/components/HooksExample/LifeCycHooks.vue"
+
+import { ref } from 'vue';
+
+const isActiveOrNot = ref(true)
+
+setTimeout(() => {
+  isActiveOrNot.value = false
+}, 5000)
+
+</script>
+
+<template>
+
+  <LifeCycHooks v-if="isActiveOrNot" />
+  
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
