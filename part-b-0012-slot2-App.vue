96fbb5768a47a1585035b852fb39f92133005c8e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-05 12:46:13 +0300

    Create part-b-0012-slot2-App.vue

---
+++ part-b-0012-slot2-App.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+import EmpSalary from "@/components/EmployeeData/EmpSalary.vue"
+import { reactive } from "vue";
+
+const employees = reactive([
+  {name: "Ayhan", id: 1, salary: 55000},
+  {name: "Hakan", id: 3, salary: 45000},
+  {name: "Kağan", id: 12, salary: 65000},
+  {name: "Bengü", id: 4, salary: 42000}
+])
+
+</script>
+
+<template>
+  <app-header />
+
+  <h3>Employee Name and Salary</h3>
+
+  <EmpSalary>
+
+    <template v-slot:empsalaries>
+      <ul>
+      <li v-for="emp in employees" :key="emp.id">
+        {{ emp.name }} : {{ emp.salary }}
+      </li>
+    </ul>
+    </template>
+
+    <template v-slot:firmarea>
+      <h3>Web3 and AI Solutions</h3>
+    </template>
+
+  </EmpSalary>
+ 
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
