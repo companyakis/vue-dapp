cf7391ea28e64b853c4bb25c863b04b96d054be6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-05 13:42:11 +0300

    Create part-b-0014-lifecycle-hooks1-LifeCycHooks.vue

---
+++ part-b-0014-lifecycle-hooks1-LifeCycHooks.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import { onBeforeMount, onMounted } from 'vue';
+
+onBeforeMount(() => {console.log("First step!")})
+
+onMounted(() => {console.log("Second step!")})
+
+</script>
+
+<template>
+   
+</template>
