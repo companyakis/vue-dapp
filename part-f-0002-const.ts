8dca4ab97e81a2eeaead5c0995d1b5b76987877a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-06 00:28:16 +0300

    Create part-f-0002-const.ts

---
+++ part-f-0002-const.ts
@@ -0,0 +1,17 @@
+// onst, the variable must be initialized at the time of declaration, and it cannot be reassigned later
+
+const me: string = "Mustafa Büyükdereli"
+
+// me = "Who am I?" // Cannot assign to 'me' because it is a constant.ts(2588)
+
+const pi: number = 22 / 7
+
+console.log(me)
+
+console.log(pi)
+
+/*
+Mustafa Büyükdereli
+3.142857142857143
+*/
+
