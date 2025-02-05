cb2eaf559519a7ed90e0bedcdb7039398f195732
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-05 13:08:06 +0300

    Create part-b-0013-dynamic-component1-App.vue

---
+++ part-b-0013-dynamic-component1-App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import Proverb1 from '@/components/Proverbs/Proverb1.vue';
+import Proverb2 from '@/components/Proverbs/Proverb2.vue';
+
+import { shallowRef } from 'vue';
+
+const activeComponent = shallowRef(Proverb1)
+
+</script>
+
+<template>
+
+  <div>
+    <button @click="activeComponent = Proverb1">English Proverb</button>
+    <button @click="activeComponent = Proverb2">Türk Atasözü</button>
+  </div>
+  
+  <div>
+    <component :is="activeComponent"></component>
+  </div>
+
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
