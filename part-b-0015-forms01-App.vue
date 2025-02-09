783b4a58733113e6920a2fe98ab643f35bc3c681
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 00:45:32 +0300

    Create part-b-0015-forms01-App.vue

---
+++ part-b-0015-forms01-App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import EmployeeContact from './components/EmpContact/EmployeeContact.vue';
+
+import Header from './components/Fixed/Header.vue';
+
+
+</script>
+
+<template>
+  <Header />
+
+  <EmployeeContact />
+  
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
