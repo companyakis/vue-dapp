4d600a75bcf3960563b75f684d0d7c148ea84b0b
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 21:09:53 +0300

    Create part-c-0013-constant.sol

---
+++ part-c-0013-constant.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Mustafa {
+
+    // Constants can only be used for the definition of state variables
+    // Constants are typically represented in uppercase
+
+    uint constant ABSOLUTE_BUDGET_USD = 550000;
+
+    uint16 constant STARTYEAR = 2025;
+
+}
