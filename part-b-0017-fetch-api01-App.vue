4c78ef11bc60b2787183a8896bc2e85b073ab7c8
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 23:05:43 +0300

    Create part-b-0017-fetch-api01-App.vue

---
+++ part-b-0017-fetch-api01-App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import Departments from './components/DepartmentsData/Departments.vue';
+import AddDepartment from './components/DepartmentsData/AddDepartment.vue';
+
+import {shallowRef} from 'vue';
+
+const activeComponent = shallowRef(Departments)
+
+</script>
+
+<template>
+
+  <div class="container mt-5 mb-4">
+    <button @click="activeComponent = Departments" class="btn btn-success me-3">Departments</button>
+    <button @click="activeComponent = AddDepartment" class="btn btn-warning">Add a Department</button>
+  </div>
+  <div class="container">
+    <component :is="activeComponent"/>
+  </div>
+
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
