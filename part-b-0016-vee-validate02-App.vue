42c6ec851bd98c59797b43b8c725b52f6288a2ce
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 18:16:58 +0300

    Create part-b-0016-vee-validate02-App.vue

---
+++ part-b-0016-vee-validate02-App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import Proverb from './components/Proverb/Proverb.vue';
+
+</script>
+
+<template>
+
+  <Proverb />
+
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
