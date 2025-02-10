f34366bd897e911097f235e1c35ba8864938f93d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-10 17:36:46 +0300

    Create part-f-0001-welcome-App.vue

---
+++ part-f-0001-welcome-App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import { reactive } from 'vue';
+
+const data = reactive({
+  myName: "Mustafa Buyukdereli",
+  year: 2025
+})
+
+</script>
+
+<template>
+
+  <h3>
+    Vue 101 tutorial by {{ data.myName }} and year is {{ data.year }}...
+  </h3>
+  
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
