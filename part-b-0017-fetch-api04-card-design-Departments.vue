1ef6c15186730da1bd15bbfc9fbac14cab4ec8e5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-11 13:40:26 +0300

    Create part-b-0017-fetch-api04-card-design-Departments.vue

---
+++ part-b-0017-fetch-api04-card-design-Departments.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+// npm i axios
+
+import axios from 'axios';
+import { reactive, onMounted } from 'vue';
+
+const data = reactive({
+    departments: []
+})
+
+const getDepartments = async() => {
+    try {
+        const response = await axios.get("http://localhost:3000/deps")
+        //console.log(response.data)
+        data.departments = response.data
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+onMounted(() => {
+    getDepartments()
+})
+
+</script>
+
+<template>
+
+    <div class="row mt-4">
+        <div class="col-md-6 mb-3" v-for="d in data.departments" :key="d.id">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-text">
+                        Department: {{ d.name }}
+                    </h5>
+                    <p class="card-text">
+                        Department Head: {{ d.head }}
+                    </p>
+                    <p class="card-text">
+                        Department Head: {{ d.head }}
+                    </p>
+                    <p class="card-text">
+                        Number of Employees: {{ d.employees }}
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    
+</template>
