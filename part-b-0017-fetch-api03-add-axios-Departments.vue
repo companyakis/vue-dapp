5f701e42ac156d463c60e854236add289a3765a3
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-11 13:29:41 +0300

    Create part-b-0017-fetch-api03-add-axios-Departments.vue

---
+++ part-b-0017-fetch-api03-add-axios-Departments.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+// npm i axios
+
+import axios from 'axios';
+import { reactive, onMounted } from 'vue';
+
+const data = reactive({
+    departments: []
+})
+
+const getDepartments = async() => {
+    try {
+        const response = await axios.get("http://localhost:3000/deps")
+        //console.log(response.data)
+        data.departments = response.data
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+onMounted(() => {
+    getDepartments()
+})
+
+</script>
+
+<template>
+    
+</template>
