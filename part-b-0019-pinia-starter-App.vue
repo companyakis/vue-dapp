c09101d2e709846c45cf35f662b74d1439810054
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-02-13 21:11:08 +0300

    Create part-b-0019-pinia-starter-App.vue

---
+++ part-b-0019-pinia-starter-App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import { useCounterStore } from './stores/counter';
+
+const store = useCounterStore()
+
+</script>
+
+<template>
+
+  <p>{{ store.counter }}</p>
+
+  <p>{{ store.counterData.version }}</p>
+
+  <p>{{ store.counterData.createdBy }}</p>
+  
+</template>
+
